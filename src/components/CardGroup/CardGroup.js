22f0fe3ef816388f2425fbfef4d6d8103d5d7832
arfan36 <arfanur36@gmail.com>
2022-10-03 16:17:35 +0600

    51-1: Module Introduction Use Bootstrap CDN in React

---
+++ src/components/CardGroup/CardGroup.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Card from '../Card/Card';
+
+const CardGroup = () => {
+    const products = [
+        { id: 1, name: 'Mac Book Pro', price: 145000 },
+        { id: 2, name: 'Mac Book Crow', price: 155000 },
+        { id: 3, name: 'Mac Book Grow', price: 165000 }
+    ];
+
+    return (
+        <div className="card-group">
+            {
+                products.map(product => <Card
+                    key={product.id}
+                    product={product}
+                ></Card>)
+            }
+        </div>
+    );
+};
+
+export default CardGroup;
